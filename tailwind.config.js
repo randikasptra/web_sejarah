739c25409ac106660968b32c2f77b9461d886195
Muhammad Randika <164119317+randikasptra@users.noreply.github.com>
2024-10-12 17:06:46 +0700

    awal menambah file

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ['./index.html','./article.html' ,'./src/**/*.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary : ["#493628"],
+        secondary : ["#AB886D"],
+        ketiga : ["#D6C0B3"],
+        putih : ["#E4E0E1"]
+      },
+      fontFamily: {
+        delta: ['Dela Gothic One', 'sans-serif'],
+        sora : ['Sora'],
+        apacad : ['Afacad Flux']
+      },
+      backgroundImage: {
+        'h-product': "url('./img/hero-tentangkami.png')"
+      },
+      
+    },
+  },
+  plugins: [],
+};
